d19cb26fedfa39de5bb688cb3b7329268d54f963
Devonte <devo.tox.89@gmail.com>
2019-03-06 11:25:29 +0000

    update ember

--- README.md
+++ README.md
@@ -23,7 +23,6 @@ Installation
 ember install ember-cli-nearley
 ```
 
-
 Usage
 ------------------------------------------------------------------------------
 
@@ -75,29 +74,7 @@ OR
 Contributing
 ------------------------------------------------------------------------------
 
-### Installation
-
-* `git clone <repository-url>`
-* `cd ember-cli-nearley`
-* `yarn install`
-
-### Linting
-
-* `yarn lint:js`
-* `yarn lint:js --fix`
-
-### Running tests
-
-* `ember test` – Runs the test suite on the current Ember version
-* `ember test --server` – Runs the test suite in "watch mode"
-* `ember try:each` – Runs the test suite against multiple Ember versions
-
-### Running the dummy application
-
-* `ember serve`
-* Visit the dummy application at [http://localhost:4200](http://localhost:4200).
-
-For more information on using ember-cli, visit [https://ember-cli.com/](https://ember-cli.com/).
+See the [Contributing](CONTRIBUTING.md) guide for details.
 
 License
 ------------------------------------------------------------------------------
--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 'use strict';
 
 const fs = require('fs');
+const { name } = require('./package');
+
 const nearley = require('nearley');
 const compile = require('nearley/lib/compile');
 const generate = require('nearley/lib/generate');
 const nearleyGrammar = require('nearley/lib/nearley-language-bootstrapped');
+
+module.exports = {
+    name,
+	options: {
+		'ember-cli-babel': {
+			compileModules: true,
+			includePolyfill: true,
+			disableDebugTooling: true
+		}
+	},
+
+	included() {
+		this._super.included.apply(this, arguments);
+
+		this.import('vendor/shims/moo.js');
+		this.import('vendor/shims/nearley.js');
+
+		this.import('vendor/shims/grammar-tokens.js'); // must be before grammar
+		this.import('vendor/shims/grammar.js');
+
+		this.import('node_modules/moo/moo.js');
+		this.import('node_modules/nearley/lib/nearley.js');
+
+		const compiled = compileGrammar();
+		const sourceCreated = compiled[0];
+		const tokensCreated = compiled[1];
+
+		if(tokensCreated) { this.import('vendor/grammar-tokens.js'); }
+		if(sourceCreated) { this.import('vendor/grammar.js'); }
+
+	}
+};
+
+
 
 const compileGrammar = () => {
 	let ret = [false, false];
@@ -36,37 +72,4 @@ const compileGrammar = () => {
 	}
 
 	return ret;
-};
-
-module.exports = {
-	name: 'ember-cli-nearley',
-
-	options: {
-		'ember-cli-babel': {
-			compileModules: true,
-			includePolyfill: true,
-			disableDebugTooling: true
-		}
-	},
-
-	included() {
-		this._super.included.apply(this, arguments);
-
-		this.import('vendor/shims/moo.js');
-		this.import('vendor/shims/nearley.js');
-
-		this.import('vendor/shims/grammar-tokens.js'); // must be before grammar
-		this.import('vendor/shims/grammar.js');
-
-		this.import('node_modules/moo/moo.js');
-		this.import('node_modules/nearley/lib/nearley.js');
-
-		const compiled = compileGrammar();
-		const sourceCreated = compiled[0];
-		const tokensCreated = compiled[1];
-
-		if(tokensCreated) { this.import('vendor/grammar-tokens.js'); }
-		if(sourceCreated) { this.import('vendor/grammar.js'); }
-
-	}
 };
